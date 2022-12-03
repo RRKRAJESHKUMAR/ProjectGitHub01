3245eebec784215f05982383db4dc691f29e339c
RRKRAJESHKUMAR <kumarrajesh99403@gmail.com>
2022-12-03 14:19:09 +0530

    RoughGit Project

---
+++ src/test/java/com/maven/ProjectGit.java
@@ -0,0 +1,19 @@
+package com.maven;
+
+public class ProjectGit {
+
+	
+	
+	private void method() {
+		// TODO Auto-generated method stub
+
+	}
+	private void metod1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method3() {
+		// TODO Auto-generated method stub
+
+	}
+}
